c058dfab8ba8c388f53a543867e6c1e3e67c302f
Leslie Hoare <iam@lesleh.co.uk>
2013-10-09 00:02:24 +0100

    Add rot13.

---
+++ rot13/rot13.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace rot13
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string line;
+			while((line = Console.In.ReadLine()) != null) {
+				Console.WriteLine(rot13(line));
+			}
+		}
+
+		public static string rot13 (string value)
+		{
+			char[] array = value.ToCharArray();
+			for(int i = 0; i < array.Length; i++){
+				int number = (int)text[i];
+				if(number >= 'a' && number <= 'm' || number >= 'A' && number <= 'M') {
+					number += 13;
+				} else if(number >= 'm' && number <= 'z' || number >= 'M' && number <= 'Z') {
+					number -= 13;
+				}
+				array[i] = (char)number;
+			}
+			return new string(array);
+		}
+	}
+}
