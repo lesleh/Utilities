acab954474a2b92ffe4ce40da2cacb4ba75c9531
Leslie Hoare <iam@lesleh.co.uk>
2013-11-27 02:13:59 +0000

    Better wordwrap implementation.

--- wordwrap/wordwrap.cs
+++ wordwrap/wordwrap.cs
@@ -1,14 +1,17 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Text;
 
 namespace wordwrap
 {
 	class MainClass
 	{
+		private static int WRAP_LENGTH = 80;
+
 		public static void Main (string[] args)
 		{
-			Wrap(Console.In, Console.Out, 80, true);
+			Wrap(Console.In, Console.Out, WRAP_LENGTH, true);
 		}
 
 		private static void Wrap(TextReader input, TextWriter output, int count, bool force)
@@ -16,39 +19,56 @@ namespace wordwrap
 			string line;
 			while((line = input.ReadLine()) != null)
 			{
-				string[] parts = SplitString(line, count, force);
-				foreach(string part in parts)
-				{
-					output.WriteLine(part);
-				}
+				string wrapped = SplitString(line, count, force);
+				output.Write(wrapped);
 			}
 		}
 
-		private static string[] SplitString(string input, int count, bool force)
+		private static string SplitString(string str, int wrapLength, bool wrapLongWords)
 		{
-			var parts = new List<string>();
+			StringBuilder wrappedLine = new StringBuilder(str.Length + 32);
 
-			// TODO Naive implementation, fix later
-			int start = 0;
-			int end = (input.Length < count) ? input.Length : count;
-			while(start < input.Length) {
-				if(end < input.Length) {
-					while(!char.IsWhiteSpace(input[end - 1]))
-						end--;
+			int inputLineLength = str.Length;
+			int offset = 0;
+
+			while ((inputLineLength - offset) > wrapLength) {
+				if (str[offset] == ' ') {
+					offset++;
+					continue;
 				}
-				parts.Add(SafeSubstring(input, start, end - start));
-				start = end;
-				end += count;
+				int spaceToWrapAt = str.LastIndexOf(' ', wrapLength + offset);
+
+				if (spaceToWrapAt >= offset) {
+					// normal case
+					wrappedLine.Append(str.Substring(offset, spaceToWrapAt - offset));
+					wrappedLine.Append('\n');
+					offset = spaceToWrapAt + 1;
+				}/* else {
+					// really long word or URL
+					if (wrapLongWords) {
+						// wrap really long word one line at a time
+						wrappedLine.append(str.substring(offset, wrapLength + offset));
+						wrappedLine.append(newLineStr);
+						offset += wrapLength;
+					} else {
+						// do not wrap really long word, just extend beyond limit
+						spaceToWrapAt = str.indexOf(' ', wrapLength + offset);
+						if (spaceToWrapAt >= 0) {
+							wrappedLine.append(str.substring(offset, spaceToWrapAt));
+							wrappedLine.append(newLineStr);
+							offset = spaceToWrapAt + 1;
+						} else {
+							wrappedLine.append(str.substring(offset));
+							offset = inputLineLength;
+						}
+					}
+				}*/
 			}
 
-			return parts.ToArray();
-		}
+			// Whatever is left in line is short enough to just pass through
+			wrappedLine.Append(str.Substring(offset));
 
-		public static string SafeSubstring(string text, int start, int length)
-		{
-			return text.Length <= start ? string.Empty
-					: text.Length - start <= length ? text.Substring(start)
-					: text.Substring(start, length);
+			return wrappedLine.ToString();
 		}
 	}
 }
